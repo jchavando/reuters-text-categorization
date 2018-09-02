633f17d77d7c14b7d8d7eb55a3124caa6cba6dc1
Julie Chavando <Julie@Julie-Chavandos-MacBook-Pro.local>
2018-09-02 16:14:02 -0700

    initializations

--- reuters_loader.py
+++ reuters_loader.py
@@ -17,3 +17,58 @@
 # 	$ python reuters_loader.py
 
 #common packes for extracting and transforming Machine Learning data
+from collections import defaultdict
+import csv
+import json
+import numpy as np
+import os
+import pandas as pd
+import xml.sax #for markup language transformation
+from xml.sax.handler import ContentHandler
+ 
+# object that holds all values for each individual article
+class Article(object):
+	#each value is initialized to an empty value because not every article has each tag/content present
+	def __init__(self):
+		self.body = ""
+		self.data = ""
+		self.title = ""
+		self.topics = []
+		self.places = []
+		self.people = []
+		self.author = ""
+		self.dateline = ""
+		self.exchanges = []
+		self.companies = []
+		self.orgs = []
+		self.mknote = ""
+
+class MyHandler(ContentHandler):
+	def __init__(self):
+		self.count = 1
+		self.tag = ''
+		self.defaultdict = defaultdict(list)
+		self.topdict = defaultdict(list)
+		self.boddict = defaultdict(list)
+		self.titledict = defaultdict(list)
+		self.lewissplit = list()
+		self.article = Article()
+		self.df = pd.DataFrame()
+		self.in_d = False
+		self.docID = 0
+		self.lewis = "train"
+		self.freqDict = defaultdict(int)
+		#get frequency count of most popular topics
+		file = open("all_topics.csv")
+		for word in file.read().split():
+			self.freqDict[word] += 1
+
+	#booleans that keep track of the tags that SAX does not parse correctly and therefore need to be cleaned
+	def _reset(self):
+		self.in_places = False
+		self.in_topics = False
+		self.in_people = False
+		self.in_exchanges = False
+		self.in_companies = False
+		self.in_orgs = False
+	
